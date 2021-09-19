6badef328be05e71c2d59774f2a424e121fee542
Washington Correia <washingtonkle@gmail.com>
2021-09-19 16:36:26 -0300

    fix: email validation by registered user

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from 'tsyringe';
+import  { hash } from 'bcrypt';
+
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -11,10 +13,19 @@ class CreateUserUseCase {
     ) {}
     
     async execute({ name, email, password }: ICreateUserDTO): Promise<void> {
+
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new Error('E-mail já foi cadastrado!');
+        }
+
+        const encryptPassword = await hash(password, 8);
+
         await this.usersRepository.create({
             name,
             email,
-            password
+            password: encryptPassword
         })
     }
 }
