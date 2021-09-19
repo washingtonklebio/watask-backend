a906c0276800ffe41be8ba24573ad5193bb23256
Washington Correia <washingtonkle@gmail.com>
2021-09-19 17:51:57 -0300

    feat: user authentication implementation

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -3,6 +3,7 @@ import  { hash } from 'bcrypt';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from '../../../../errors/AppError';
 
 @injectable()
 class CreateUserUseCase {
@@ -17,7 +18,7 @@ class CreateUserUseCase {
         const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
         if (userAlreadyExists) {
-            throw new Error('E-mail já foi cadastrado!');
+            throw new AppError('E-mail já foi cadastrado!');
         }
 
         const encryptPassword = await hash(password, 8);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 // Routes
 import { usersRouters } from './users.routes';
+import { authenticateRouters } from './authenticate.routes';
 
 const router = Router();
 
 router.use('/users', usersRouters);
+router.use(authenticateRouters);
 
 
 export { router }
