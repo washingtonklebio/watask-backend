47602896697cec1550768b68141346daf2c1f774
Washington Correia <washingtonkle@gmail.com>
2021-09-21 01:07:44 -0300

    chore: settings orm

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
-    "entities": ["./src/modules/**/entities/*.ts"], // local ts
-    "migrations": ["./src/database/migrations/*.ts"], // local ts
+    "entities": ["./src/modules/**/entities/*.js"], // local ts
+    "migrations": ["./src/database/migrations/*.js"], // local ts
     // "migrations": ["./src/database/migrations/*.ts"],
     // "entities": ["./src/modules/**/entities/*.ts"], 
     "cli": {
